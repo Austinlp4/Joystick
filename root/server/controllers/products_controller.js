e9d2a498d2631d886bb21e9e955b903876fcf8b7
Austinlp4 <apendergrast44@gmail.com>
2019-01-31 15:57:43 -0600

    read all products

--- root/server/controllers/products_controller.js
+++ root/server/controllers/products_controller.js
@@ -1,2 +1,11 @@
+const Product = require('../models/product');
+
 module.exports = {
+    readAllProducts(req, res){
+        Product.find({}).exec((err, products) => {
+            if(err) console.log('Error===>', err);
+        console.log('products', products);
+        res.status(200).send(products)
+        })
+    }
 };
