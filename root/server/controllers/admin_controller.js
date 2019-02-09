5429acae9677fd817a7f009f05ea54f65edac722
Austinlp4 <apendergrast44@gmail.com>
2019-02-09 10:42:57 -0600

    update

--- root/server/controllers/admin_controller.js
+++ root/server/controllers/admin_controller.js
@@ -1,2 +1,12 @@
+const Product = require('../models/product');
+const User = require('../models/user');
+
+
 module.exports = {
+    getAdminUsers(req, res) {
+        User.find().exec((err, users) => {
+        if(err) console.log('Find Admin Users Error---------------', err);
+        res.status(200).json({users});
+       })
+      }, 
 };
--- root/server/controllers/products_controller.js
+++ root/server/controllers/products_controller.js
@@ -7,5 +7,16 @@ module.exports = {
         console.log('products', products);
         res.status(200).send(products)
         })
-    }
+    },
+    readProduct(req, res) {
+        //Destruct the id from the endpoint url, to retrieve  a specific product.
+        const { id } = req.params;
+       //Copy and paste on of the product's id to the url when testing it.
+       //Use the findById to get a specific product.
+       Product.findById(id).exec((err, product) => {
+        if(err) console.log('Get Single Product Error---------------', err);
+        console.log('product--------------', product);
+        res.status(200).json({product});
+       })
+      }
 };
